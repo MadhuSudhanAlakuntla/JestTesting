cd52798ee9aa9ae53d68d909f2efc4425b88efa7
Madhusudhanalakuntla <madhusudhan03833@gmail.com>
2024-08-08 14:23:13 +0530

    done

---
+++ myapp/src/Sample.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from './logo.svg';
+function Sample() {
+    return (
+        <>
+        <h2>Sample</h2>
+        <img src={logo} alt="React Logo" />
+        </>
+        
+    )
+}
+
+export default Sample
